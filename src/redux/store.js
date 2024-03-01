7f0997aba923e4225aac058a958cfc27cbb5b9ee
Andrii <rod.andriiw@gmail.com>
2024-03-01 16:37:05 +0000

    add-gitkeep

---
+++ src/redux/store.js
@@ -0,0 +1,38 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistStore,
+  persistReducer,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['token'],
+};
+
+// const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactReducer,
+    filter: filterReducer,
+    auth: persistedAuthReducer,
+  },
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  ],
+});
+
+export const persistor = persistStore(store);
