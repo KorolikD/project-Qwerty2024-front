dc8ade66d9e78c6aa080a71e2b16a8f9486e4806
Andrii <rod.andriiw@gmail.com>
2024-03-01 15:10:38 +0000

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
-import SharedLayout from 'components/SharedLayout/SharedLayout';
-import WelcomePage from 'pages/WelcomePage/WelcomePage';
-import SignUpPage from 'pages/SignUpPage/SignUpPage';
-import HalfPage from 'pages/HalfPage/HalfPage';
-import ErrorPage from 'pages/ErrorPage/ErrorPage';
-import { AppWrapper } from './App.styled';
-
-const test = import.meta.env.VITE_API_TEST;
-
 function App() {
-  console.log(test);
-  return (
-    <AppWrapper>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route path="/first" element={<WelcomePage />} />
-          <Route path="/second" element={<SignUpPage />}>
-            <Route path=":half" element={<HalfPage />} />
-          </Route>
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
-    </AppWrapper>
-  );
+
+  return <></>
 }
 export default App;
---
+++ src/pages/404Page/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import { Container, Title } from './ErrorPage.styled.js';
+
+const ErrorPage = () => {
+  return (
+    <Container>
+      <Title>404</Title>
+    </Container>
+  );
+};
+
+export default ErrorPage;
