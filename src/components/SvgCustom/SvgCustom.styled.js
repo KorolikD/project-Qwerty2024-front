0995b077e4dcaec52f3ad56c98fd6856923c1139
Igor Liakh <igorlyah060@gmail.com>
2024-03-02 23:19:47 +0200

    fix svg component

--- src/components/SvgCustom/SvgCustom.styled.js
+++ src/components/SvgCustom/SvgCustom.styled.js
@@ -4,8 +4,22 @@ export const SvgWrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: ${(props) => props.background};
+  color: ${(props) => props.theme.colors.primary};
   border-radius: 100px;
   width: ${(props) => props.width}px;
   height: ${(props) => props.height}px;
+  svg {
+    fill: ${(props) => {
+      if (
+        props.svgName === 'icon-calendar' ||
+        props.svgName === 'icon-logout' ||
+        props.svgName === 'icon-settings' ||
+        props.svgName === 'icon-trash'
+      ) {
+        return 'none';
+      }
+      return 'currentColor';
+    }};
+    stroke: currentColor;
+  }
 `;
