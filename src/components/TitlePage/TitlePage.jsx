c76636705940e7ef65814beab6953d196c678007
dariameln1 <dariamelnykova1@gmail.com>
2024-03-04 17:35:38 +0100

    commit

---
+++ src/components/TitlePage/TitlePage.jsx
@@ -0,0 +1,7 @@
+import { Title } from './TitlePage.styled';
+
+const TitlePage = ({ title }) => {
+  return <Title>{title}</Title>;
+};
+
+export default TitlePage;
---
+++ src/components/TitlePage/TitlePage.styled.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const TitleWrapper = styled.div`
+    @media screen and (max-width: 375px) {
+
+  }
+    @media screen and (min-width: 768px) {
+    
+  }
+    @media screen and (min-width: 1440px) {
+  
+  }
+`;
+
+export const Title = styled.h1`
+    font-size: 24px;
+  @media screen and (max-width: 375px) {
+
+  }
+  @media screen and (min-width: 768px) {
+    
+  }
+  @media screen and (min-width: 1440px) {
+  
+  }
+`;
+
