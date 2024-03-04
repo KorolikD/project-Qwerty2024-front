2eb93ce66cfe467e447647742979970d51756768
Igor Liakh <igorlyah060@gmail.com>
2024-03-04 22:11:58 +0200

    added fonts

--- src/styles/global.styled.js
+++ src/styles/global.styled.js
@@ -1,6 +1,30 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    src: url('../fonts/roboto-v30-latin-regular.woff2') format('woff2'); 
+  }
+  @font-face {
+    font-display: swap; 
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 500;
+    src: url('../fonts/roboto-v30-latin-500.woff2') format('woff2'); 
+  }
+  @font-face {
+    font-display: swap; 
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    src: url('../fonts/roboto-v30-latin-700.woff2') format('woff2'); 
+  }
+
+
   body {
     font-family: ${({ theme }) => theme.fonts.mainFont};
     background-color: ${({ theme }) => theme.colors.black};
