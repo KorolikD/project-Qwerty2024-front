8bee9a81f90f0503151fa3f348f1af4891e32375
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2024-03-06 17:10:32 +0200

    added DayDashboard and basic logic on DiaryPage

--- src/components/DayDashboard/DayDashboard.jsx
+++ src/components/DayDashboard/DayDashboard.jsx
@@ -1,3 +1,96 @@
-export const DayDashboard = () => {
-  return <></>;
-};
+import SvgCustom from '../SvgCustom/index.js';
+import theme from '../../styles/theme.js';
+import { AttentionDiv, DashboardItem, DashboardList, DashboardSection, DashboardText, SvgTextWrap } from './DayDashboard.styled.js';
+
+const DayDashboard = () =>
+  // {bmr,
+  // dpa,
+  // caloriesConsumed='0',
+  // caloriesBurned='0,
+  // restOfCalories,
+  // restOfSports,}
+  {
+    return (
+      <DashboardSection>
+        <DashboardList>
+          <DashboardItem>
+            <SvgTextWrap>
+              <SvgCustom
+                icon="icon-food"
+                size="20"
+                color={theme.colors.secondary}
+              />
+              <p>Daily calorie intake</p>
+            </SvgTextWrap>
+            <DashboardText>bmr</DashboardText>
+          </DashboardItem>
+          <DashboardItem>
+            <SvgTextWrap>
+              <SvgCustom
+                icon="icon-dumbbell"
+                size="20"
+                color={theme.colors.secondary}
+              />
+              <p>Daily physical activity</p>
+            </SvgTextWrap>
+            <DashboardText>dpa min</DashboardText>
+          </DashboardItem>
+          <DashboardItem>
+            <SvgTextWrap>
+              <SvgCustom
+                icon="icon-apple"
+                size="20"
+                color={theme.colors.secondary}
+              />
+              <p>Calories consumed</p>
+            </SvgTextWrap>
+            <DashboardText>caloriesC</DashboardText>
+          </DashboardItem>
+          <DashboardItem>
+            <SvgTextWrap>
+              <SvgCustom
+                icon="icon-apple"
+                size="20"
+                color={theme.colors.secondary}
+              />
+              <p>Calories burned</p>
+            </SvgTextWrap>
+            <DashboardText>caloriesB</DashboardText>
+          </DashboardItem>
+          <DashboardItem>
+            <SvgTextWrap>
+              <SvgCustom
+                icon="icon-bubble"
+                size="20"
+                color={theme.colors.secondary}
+              />
+              <p>Calories remaining</p>
+            </SvgTextWrap>
+            <DashboardText>restOfC</DashboardText>
+          </DashboardItem>
+          <DashboardItem>
+            <SvgTextWrap>
+              <SvgCustom
+                icon="icon-running"
+                size="20"
+                color={theme.colors.secondary}
+              />
+              <p>Sports remaining</p>
+            </SvgTextWrap>
+            <DashboardText>rest min</DashboardText>
+          </DashboardItem>
+        </DashboardList>
+        <AttentionDiv>
+          <div>
+            <SvgCustom icon="icon-next" size="24" color={theme.colors.white} />
+          </div>
+          <p>
+            Record all your meals in the calorie diary every day. This will help
+            you be aware of your nutrition and make informed choices.
+          </p>
+        </AttentionDiv>
+      </DashboardSection>
+    );
+  };
+
+export default DayDashboard;
--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -1,5 +1,70 @@
+// import { useEffect, useState } from 'react';
+// import { useSelector } from 'react-redux';
+// import dayjs from 'dayjs';
+// import { selectUser } from '../../redux/auth/authSelectors.js';
+// import { getDayInfo } from '../../services/api/diary.js';
+import DaySwitch from '../../components/DaySwitch/index.js';
+import DayDashboard from '../../components/DayDashboard/index.js';
+import { DiaryContainer, DiaryWrapper } from './DiaryPage.styled.js';
+
+// const DATE_FORMAT = 'DD/MM/YYYY';
+
 const DiaryPage = () => {
-  return <>Diary</>;
+  // const [date, setDate] = useState(dayjs().format(DATE_FORMAT));
+  // const [exercisesList, setExercisesList] = useState(null); // 1. burnedCalories (спалені під час тренування); 2. totalTime (час в хв витрачений на тренування)
+
+  // const [productsList, setProductsList] = useState(null); // 1. totalCalories (кількість спожитих за день)
+
+  // const user = useSelector(selectUser); // 1.bmr - норма калорій (за день); 2. dpa (денна норма часу тренування хв); 3.blood; 4. createdAt - дата реєстрації const {createdAt, blood, bmr, dpa} = useSelector(selectUser);
+
+  // const restOfCalories = bmr - productsList.totalCalories;
+  // const restOfSports = dpa - exercisesList.totslTime;
+
+  // useEffect(() => {
+  //   if (date === null) {
+  //     return;
+  //   }
+  //   const fetchDayInfo = async () => {
+  //     try {
+  //       const resp = await getDayInfo(date);
+  //       if (!resp) {
+  //         return;
+  //       }
+  //       setExercisesList(resp.userExercisesDiary);
+  //       setProductsList(resp.userProducts.Diary);
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   };
+  //   fetchDayInfo();
+  // }, [date]);
+
+  // const onChangeDate = (newDate) => {
+  //   setDate(newDate);
+  // };
+
+  return (
+      <DiaryWrapper>
+        <div>
+          <div>
+            Title + <DaySwitch />
+          </div>
+        </div>
+        <DiaryContainer>
+          <DayDashboard
+          //bmr={bmr}
+          //dpa={dpa}
+          //caloriesConsumed={productList.totalCalories}
+          //caloriesBurned={exercisesList.burnedCalories}
+          //restOfCalories={restOfCalories}
+          //restOfSports={restOfSports}
+          />
+          <div>
+            <span>ProductsTable</span> <span>ExercisesTable</span>
+          </div>
+        </DiaryContainer>
+      </DiaryWrapper>
+  );
 };
 
 export default DiaryPage;
