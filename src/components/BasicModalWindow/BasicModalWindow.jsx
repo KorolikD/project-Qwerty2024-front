deccf79a96507d25bd742b436d97f00f9494c197
Velms <glebmari1@gmail.com>
2024-03-09 17:36:16 +0200

    in process

--- src/components/BasicModalWindow/BasicModalWindow.jsx
+++ src/components/BasicModalWindow/BasicModalWindow.jsx
@@ -1,3 +1,20 @@
-export const BasicModalWindow = () => {
-  return <></>;
+import {
+  CloseModalBtn,
+  ModalBackdrop,
+  ModalWindow,
+} from './BasicModalWindow.styled';
+
+const BasicModalWindow = ({ children }) => {
+
+
+  return (
+    <ModalBackdrop>
+      <ModalWindow>
+        <CloseModalBtn>
+        </CloseModalBtn>
+      </ModalWindow>
+    </ModalBackdrop>
+  );
 };
+
+export default BasicModalWindow;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -11,6 +11,7 @@ const theme = {
     formBorder: 'rgba(239, 237, 232, 0.3)',
     overlay: 'rgba(4, 4, 4, 0.4)',
     grey: '#636366',
+    orange: '#E6533C',
   },
   fonts: {
     mainFont: 'Roboto, sans-serif',
