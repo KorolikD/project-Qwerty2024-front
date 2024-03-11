975f0bf7bb78413e17301b09439ee3d64c96ce65
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2024-03-11 13:16:34 +0200

    update UserCard styled

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,7 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { updateAvatar } from '../../redux/auth/authOperations.js';
 import { selectUser } from '../../redux/auth/authSelectors';
-import { Container, AvatarContainer, Avatar, UserSvg, Label, UserName, SubTitle, AddBtn, AddSvg  } from './UserCard.styled.js';
+import {
+  Container,
+  AvatarContainer,
+  Avatar,
+  UserSvg,
+  Label,
+  UserName,
+  SubTitle,
+  AddBtn,
+  AddSvg,
+} from './UserCard.styled.js';
 import sprite from '../../img/sprite.svg';
 import LogOutBtn from '../../components/LogOutBtn/LogOutBtn';
 // import StatisticsInfo from '../../components/StatisticsInfo/StatisticsInfo';
@@ -9,42 +19,45 @@ import LogOutBtn from '../../components/LogOutBtn/LogOutBtn';
 const UserCard = () => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
-  const avatarDef = ( <UserSvg><use href={`${sprite}#icon-user`}></use></UserSvg>);
-  const avatarImg = (<Avatar src={user.avatarUrl} alt="Avatar"/>)
+  const avatarDef = (
+    <UserSvg>
+      <use href={`${sprite}#icon-user`}></use>
+    </UserSvg>
+  );
+  const avatarImg = <Avatar src={user.avatarUrl} alt="Avatar" />;
   const avatarShown = user.avatarUrl ? avatarImg : avatarDef;
 
   const handleAvatarChange = (e) => {
     const file = e.target.files[0];
     if (file) {
-      dispatch(updateAvatar(file)); 
+      dispatch(updateAvatar(file));
     }
-  };
-
-  const openFileInput = () => {
-    const fileInput = document.getElementById("fileInput");
-    fileInput.click();
   };
 
   return (
     <Container>
       <AvatarContainer>
         {avatarShown}
+        <AddBtn>
+          <Label htmlFor="fileInput">
+            <AddSvg>
+              <use
+                href={`${sprite}#icon-add-avatar`}
+                aria-label="Upload Avatar"
+              ></use>
+            </AddSvg>
+          </Label>
+          <input
+            type="file"
+            accept=".jpg, .jpeg"
+            id="fileInput"
+            name="fileInput"
+            style={{ display: 'none' }}
+            onChange={handleAvatarChange}
+          />
+        </AddBtn>
       </AvatarContainer>
-      <AddBtn>
-        <Label htmlFor="fileInput" onClick={openFileInput}>
-          <AddSvg>
-            <use href={`${sprite}#icon-add-avatar`} aria-label="Upload Avatar"></use>
-          </AddSvg>
-        </Label>
-        <input 
-          type="file" 
-          accept=".jpg, .jpeg" 
-          id="fileInput" 
-          name="fileInput"
-          style={{ display: 'none' }}
-          onChange={handleAvatarChange} 
-        />
-      </AddBtn>
+
       <UserName>{user.name}</UserName>
       <SubTitle>User</SubTitle>
       {/* <StatisticsInfo/> */}
