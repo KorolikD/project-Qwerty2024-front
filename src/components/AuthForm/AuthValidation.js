53efeb7615d9c4350f9f39a737efef7dbab357b7
Maria Novosad <100295231+avemasha@users.noreply.github.com>
2024-03-11 22:12:48 +0100

    emailVaalidationupd

--- src/components/AuthForm/AuthValidation.js
+++ src/components/AuthForm/AuthValidation.js
@@ -1,14 +1,20 @@
 import * as Yup from 'yup';
 
+const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
 export const signUpValidationSchema = Yup.object({
   name: Yup.string().required('Name is required'),
-  email: Yup.string().email('Invalid email').required('Email is required'),
+  email: Yup.string()
+    .matches(emailRegex, 'Invalid email')
+    .required('Email is required'),
   password: Yup.string()
     .min(6, 'Password must be at least 6 characters')
     .required('Password is required'),
 });
 
 export const signInValidationSchema = Yup.object({
-  email: Yup.string().email('Invalid email').required('Email is required'),
+  email: Yup.string()
+    .matches(emailRegex, 'Invalid email')
+    .required('Email is required'),
   password: Yup.string().required('Password is required'),
 });
